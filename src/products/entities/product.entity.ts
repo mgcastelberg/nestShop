7fce384af71cb9e6da0d80ab82d8dd19f7a8d46a
Manuel Gomez <jmgc@virket.com>
2024-07-11 23:37:29 -0600

    Entidad sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,5 @@
 
-import { Entity, Column, PrimaryColumn, BeforeInsert } from 'typeorm';
+import { Entity, Column, PrimaryColumn, BeforeInsert, PrimaryGeneratedColumn } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
 @Entity()
@@ -7,14 +7,37 @@ export class Product {
     @PrimaryColumn({ type: 'varchar', length: 36, default: uuidv4() })
     id: string;
 
-    @Column()
-    firstName: string;
+    @Column('varchar',{unique: true})
+    title: string;
 
-    @Column()
-    lastName: string;
+    @Column('numeric',{ default: 0 })
+    price: number;
 
-    @Column({ default: true })
-    isActive: boolean;
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @Column('varchar', { length: 255, unique: true })
+    slug: string;
+
+    @Column('int', { default: 0 })
+    stock: number;
+
+    // Only apply to postgres
+    // @PrimaryGeneratedColumn('uuid');
+    // id: string;
+
+    // @Column('text',{ array: true, default: [] })
+    // sizes: string[];
+
+    // Para mysql solo acepta json
+    @Column('json', { array: true })
+    sizes: string[];
+
+    @Column('enum', { enum: ['men', 'women', 'kid', 'unisex'] })
+    gender: string;
+
+    @Column({ type: 'decimal', nullable: false })
+    subtotal: number;
 
     @BeforeInsert()
     private setId(): void {

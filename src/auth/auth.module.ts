ff07e3e6a827793ec69e72d33c4365317219c19f
Manuel Gomez <jmgc@virket.com>
2024-08-11 21:16:27 -0600

    Módulos Asíncronos

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,6 +13,19 @@ import { User } from './entities/user.entity';
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ ConfigModule],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        // console.log('Jwt', configService.get('JWT_SECRET'));
+        // console.log('JWT_SECRET', process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '2h' },
+        }
+      }
+    }),
   ],
   exports: [ TypeOrmModule ]
 })

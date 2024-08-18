895b970587f57359861f9568466652c620414421
Manuel Gomez <jmgc@virket.com>
2024-08-17 21:20:07 -0600

    Custom Guard y Custom Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { RawHeaders } from './decorators/row-headers.decorator';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -38,6 +39,18 @@ export class AuthController {
       user,
       userEmail,
       rawHeaders
+    };
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin','super-user'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user
     };
   }
 
---
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+
+  constructor(
+    private readonly reflector: Reflector //ayuda a ver info de los decoradores
+  ) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log('UserRoleGuard');
+
+    const validRoles: string[] = this.reflector.get('roles', context.getHandler());
+
+    console.log({validRoles});
+
+    return true;
+  }
+}

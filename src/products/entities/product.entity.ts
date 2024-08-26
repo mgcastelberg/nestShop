8f704e4c194b6a72186badd23da1c1ca946d6d66
Manuel Gomez <jmgc@virket.com>
2024-08-25 18:53:06 -0600

    Expandir el ApiProperty

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,39 +7,76 @@ import { User } from 'src/auth/entities/user.entity';
 
 @Entity({ name: 'products' })
 export class Product {
-  @ApiProperty()
+  @ApiProperty({
+    example: "1bd5908e-b4f9-45c9-a3a6-00d3de2d4d12",
+    description: 'Product ID',
+    uniqueItems: true,
+    required: true
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: "T-Shirt Nestlo",
+    description: 'Product Title',
+    uniqueItems: true,
+    required: true
+  })
   @Column('varchar', { length: 255, unique: true })
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 399,
+    description: 'Product Price',
+    required: true
+  })
   @Column('float', { default: 0 })
   price: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, incididunt ut labore et dolore magna aliqua.',
+    description: 'Product Description',
+    default: null
+  })
   @Column({ type: 'text', nullable: true })
   description: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 't-shirt_Nestlo',
+    description: 'Product Slug for SEO'
+  })
   @Column('varchar', { length: 255, unique: true })
   slug: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    required: true,
+    default: 0
+  })
   @Column('int', { default: 0 })
   stock: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['S', 'M', 'L', 'XL', 'XXL'],
+    description: 'Product Sizes'
+  })
   @Column('json', { nullable: true })
   sizes: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'women',
+    description: 'Product gender',
+    required: true,
+    enum: ['men', 'women', 'kid', 'unisex']
+  })
   @Column('enum', { enum: ['men', 'women', 'kid', 'unisex'] })
   gender: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['women', 't_shirt'],
+    description: 'Product Tags'
+  })
   @Column('json', { nullable: true })
   tags: string[];
 

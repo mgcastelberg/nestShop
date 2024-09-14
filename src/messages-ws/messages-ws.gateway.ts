e0928a2c5d4d4c9cd5d7bfd1be41ad3fab955937
Manuel Gomez <jmgc@virket.com>
2024-09-13 18:30:01 -0600

    Validar JWT del Handshake

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,18 +2,31 @@ import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGa
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements  OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() wss: Server; //lo usaremos para emitir y escuchar eventos a todos los clientes
   constructor(
-    private readonly messagesWsService: MessagesWsService
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
   ) {}
   handleConnection(client: Socket) {
 
     const token = client.handshake.headers.authentication as string;
     console.log(token);
+
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify( token );
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    console.log(payload);
 
     this.messagesWsService.registerClient(client);
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients()); // emitimos a todos los clientes la lista de clientes
